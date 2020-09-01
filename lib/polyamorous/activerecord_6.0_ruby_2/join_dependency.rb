8bf02b108ae00e4261f3f962826e6d2af2ad6851
Jacob Laboissonniere <jacob.laboissonniere@carleton.ca>
2020-09-01 23:38:20 +0600

    Update join_dependency.rb

--- lib/polyamorous/activerecord_6.0_ruby_2/join_dependency.rb
+++ lib/polyamorous/activerecord_6.0_ruby_2/join_dependency.rb
@@ -29,25 +29,23 @@ module Polyamorous
     end
 
     def join_constraints(joins_to_add, alias_tracker, references)
-      @alias_tracker = alias_tracker
-      @joined_tables = {}
-      @references = {}
-      
-      references.each do |table_name|
-        @references[table_name.to_sym] = table_name if table_name.is_a?(String)
-      end unless references.empty?
-      
-      #construct_tables!(join_root)
-      joins = make_join_constraints(join_root, join_type)
+        @alias_tracker = alias_tracker
+        @joined_tables = {}
+        @references = {}
 
-      joins.concat joins_to_add.flat_map { |oj|
-        #construct_tables!(oj.join_root)
-        if join_root.match?(oj.join_root) && join_root.table.name == oj.join_root.table.name
-          walk join_root, oj.join_root, oj.join_type
-        else
-          make_join_constraints(oj.join_root, oj.join_type)
-        end
-      }
+        references.each do |table_name|
+          @references[table_name.to_sym] = table_name if table_name.is_a?(String)
+        end unless references.empty?
+
+        joins = make_join_constraints(join_root, join_type)
+
+        joins.concat joins_to_add.flat_map { |oj|
+          if join_root.match? oj.join_root
+            walk(join_root, oj.join_root, oj.join_type)
+          else
+            make_join_constraints(oj.join_root, oj.join_type)
+          end
+        }
     end
 
     private
